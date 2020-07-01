098ecf21749330d173d022238b76efac65885f4d
melaniegrigg <melanie.grigger@gmail.com>
2020-06-30 21:02:52 -0400

    add display_all method

--- app/controllers/api/products_controller.rb
+++ app/controllers/api/products_controller.rb
@@ -1,5 +1,6 @@
 class Api::ProductsController < ApplicationController
   def display_all
+    @the_products = Product.all
     render 'products.json.jb'
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,4 @@
 Rails.application.routes.draw do
-  # EXAMPLE HTML ROUTE
-  # get "/photos" => "photos#index"
-
-  # EXAMPLE JSON ROUTE WITH API NAMESPACE
   namespace :api do
     get "/all_products" => "products#display_all"
   end
