26593eecd7b1f09d29bedd2d07ecb4399a0ac41b
melaniegrigg <melanie.grigger@gmail.com>
2020-07-02 22:06:02 -0400

    add restful routes index and show

--- app/controllers/api/products_controller.rb
+++ app/controllers/api/products_controller.rb
@@ -1,3 +1,11 @@
 class Api::ProductsController < ApplicationController
-  
+  def index
+    @products = Product.all
+    render 'index.json.jb'
+  end
+
+  def show
+    @product = Product.find_by(id: params[:id])
+    render 'show.json.jb'
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
   namespace :api do
+    get "/products" => "products#index"
+    get "/products/:id" => "products#show"
   end
 end
