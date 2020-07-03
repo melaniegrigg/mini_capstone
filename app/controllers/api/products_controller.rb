3e7b9f43223ad2a60a97a294e44a39e95c2560a3
melaniegrigg <melanie.grigger@gmail.com>
2020-07-02 21:44:54 -0400

    start restful routes

--- app/controllers/api/products_controller.rb
+++ app/controllers/api/products_controller.rb
@@ -1,31 +1,3 @@
 class Api::ProductsController < ApplicationController
-  def display_all
-    @products = Product.all
-    render 'products.json.jb'
-  end
-
-  def first_product
-    @products = Product.first
-    render 'first_product.json.jb'
-  end
-
-  def last_product
-    @products = Product.last
-    render 'last_product.json.jb'
-  end
-
-  def any
-    product_id = params['products']
-    @products = Product.find_by(name: product_id)
-    render 'query_parameter.json.jb'
-  end
-
-  def guess
-    params['guess']
-    correct_number = 58
-    
-
-
-    render 'guess_number.json.jb'
-  end
+  
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,4 @@
 Rails.application.routes.draw do
   namespace :api do
-    get "/all_products" => "products#display_all"
-    get "/first_product" => "products#first_product"
-    get "/last_product" => "products#last_product"
-    get "/any_product" => "products#any"
-    get "/products_all/:products" => "products#any"
-    post "/number_guess" => "products#guess"
   end
 end
