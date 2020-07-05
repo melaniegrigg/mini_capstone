f7f21bd1a2afd514d8ce46e308ee81c816650526
melaniegrigg <melanie.grigger@gmail.com>
2020-07-05 11:29:23 -0400

    update file in controller

--- app/controllers/api/products_controller.rb
+++ app/controllers/api/products_controller.rb
@@ -11,11 +11,21 @@ class Api::ProductsController < ApplicationController
 
   def create
     @product = Product.new(
-      name: params[:name]
-      price: params[:price]
-      image_url: params[:image_url]
+      name: params[:name],
+      price: params[:price],
+      image_url: params[:image_url],
       description: params[:description]
     )
+    @product.save
+    render 'show.json.jb'
+  end
+
+  def update
+    @product = Product.find_by(id: params[:id])
+    @product.name = params[:name] || @product.name
+    @product.price = params[:price] || @product.price
+    @product.image_url = params[:image_url] || @product.image_url
+    @product.description = params[:description] || @product.description
     @product.save
     render 'show.json.jb'
   end
