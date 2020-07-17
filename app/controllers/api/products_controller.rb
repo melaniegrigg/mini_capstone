a1c68f7de247258de093234b0da63e5f7d23273c
melaniegrigg <melanie.grigger@gmail.com>
2020-07-16 20:32:50 -0400

    add category product models

--- app/controllers/api/products_controller.rb
+++ app/controllers/api/products_controller.rb
@@ -1,19 +1,11 @@
 class Api::ProductsController < ApplicationController
-  before_action: authenticate_admin, only [:create]
+  before_action :authenticate_admin, only: [:create]
 
   def index
     @products = Product.where("name LIKE ?", "%#{params[:search]}%")
 
     if params[:discount] == "true"
       @products = @products.where('price < 10')
-    end
-
-    if params[:sort] == "price" && params[:sort_order] == 'desc'
-      @products = @products.order(:price => :desc)
-    elsif params[:sort] == "price"
-      @products = @products.order(:price)    
-    else
-      @products = @products.order(:id)
     end
 
     if params[:sort] == "price" && params[:sort_order] == 'desc'
